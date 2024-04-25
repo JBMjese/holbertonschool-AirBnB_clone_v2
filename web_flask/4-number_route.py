a554482f8babcd161fc5857d237bfcc900ddf5ce
JBMjese <6935@holbertonstudents.com>
2024-04-25 15:06:40 -0500

    :sparkles: fix implentation point 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,8 +36,7 @@ def python_print(text="is cool"):
 @app.route("/number/<int:n>", strict_slashes=False)
 def num(n):
     """Function that returns a string only if n is a number"""
-    if type(n) == int:
-        return "{} is a number".format(n)
+    return f"{n} is a number"
 
 
 if __name__ == '__main__':

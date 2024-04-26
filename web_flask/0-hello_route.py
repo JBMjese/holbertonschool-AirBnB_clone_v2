b4246a360c654273e846307efb4c112a133969fd
JBMjese <6935@holbertonstudents.com>
2024-04-26 11:14:10 -0500

    :ambulance: Fix point 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Function that returns a simple string"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
